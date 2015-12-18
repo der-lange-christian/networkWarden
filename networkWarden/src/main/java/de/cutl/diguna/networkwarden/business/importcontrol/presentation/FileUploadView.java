a7356b881b65e96b7b2181534d31beb3b49afc66
Chris <der.lange.christian@gmail.com>
2015-12-18 12:39:58 +0300

    FileUpload is working

--- networkWarden/src/main/java/de/cutl/diguna/networkwarden/business/importcontrol/presentation/FileUploadView.java
+++ networkWarden/src/main/java/de/cutl/diguna/networkwarden/business/importcontrol/presentation/FileUploadView.java
@@ -1,29 +1,26 @@
-package de.cutl.diguna.networkwarden.business.importcontrol.boundary;
+package de.cutl.diguna.networkwarden.business.importcontrol.presentation;
 
+ 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
-
+ 
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
-
-/**
- *
- * @author chris
- */
+ 
 @ManagedBean
 public class FileUploadView {
-
+ 
     public void handleFileUpload(FileUploadEvent event) {
+        
         try {
             UploadedFile file = event.getFile();
+            file.write("target/test.mp3");
             
-            file.write(event.getFile().getFileName());
             
-            FacesMessage message = new FacesMessage("Succesful", file.getFileName() +
-                    " is uploaded to ");
+            FacesMessage message = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
             FacesContext.getCurrentInstance().addMessage(null, message);
         } catch (Exception ex) {
             Logger.getLogger(FileUploadView.class.getName()).log(Level.SEVERE, null, ex);
