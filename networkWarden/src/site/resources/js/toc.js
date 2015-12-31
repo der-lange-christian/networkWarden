0803b9783580c40168571436c0c089b470f7a1fc
Chris <der.lange.christian@gmail.com>
2015-12-31 18:10:46 +0300

    toc generating improved

--- networkWarden/src/site/resources/js/toc.js
+++ networkWarden/src/site/resources/js/toc.js
@@ -1,16 +1,17 @@
 
+// http://www.jankoatwarpspeed.com/automatically-generate-table-of-contents-using-jquery/
 $(document).ready(function() {
     
     $("#bodyColumn").prepend('<div id="toc"></div>');
     $("#toc").prepend('<p>In this article:</p>');
     
 
-    $("h1, h2, h3, h4, h5, h6").each(function(i) {
+    $("h3, h4, h5, h6").each(function(i) {
         var current = $(this);
         current.attr("id", "title" + i);
         $("#toc").append("<a id='link" + i + "' href='#title" +
             i + "' title='" + current.attr("tagName") + "'>" + 
-            current.text() + "</a><br />");
+            current.text() + "</a>");
         //console.log(current.html());
         //console.log(current.text());
     });
