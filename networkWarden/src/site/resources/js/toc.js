2aabd2353e9b672ebc8492195279a78fe55ee903
Chris <der.lange.christian@gmail.com>
2016-01-01 15:53:40 +0300

    table of content is minimizable and expendable

--- networkWarden/src/site/resources/js/toc.js
+++ networkWarden/src/site/resources/js/toc.js
@@ -1,15 +1,19 @@
 
+
+var toggleButtonTextOpen="table of content";
+var toggleButtonTextClose="minimize";
+
 // http://www.jankoatwarpspeed.com/automatically-generate-table-of-contents-using-jquery/
 $(document).ready(function() {
     
-    $("#bodyColumn").prepend('<div id="toc"></div>');
+    $("#bodyColumn").prepend('<div id="toc"><div id="toc_content"></div></div>');
     //$("#toc").prepend('<p>In this article:</p>');
-    
+    $("#toc").append("<a id='toggleButton' href=\"javascript:toggle('toc_content')\">" + toggleButtonTextClose + "</a>");
 
     $("h3, h4, h5, h6").each(function(i) {
         var current = $(this);
         current.attr("id", "title" + i);
-        $("#toc").append("<a id='link" + i + "' href='#title" +
+        $("#toc_content").append("<a id='link" + i + "' href='#title" +
             i + "' title='" + current.get(0).tagName + "'>" + 
             current.text() + "</a>");
         //console.log(current.html());
@@ -18,4 +22,14 @@ $(document).ready(function() {
     
 });
 
-
+function toggle(id){
+    var e = document.getElementById(id);
+     
+    if (e.style.display === "none"){
+        $("#toggleButton").text(toggleButtonTextClose);
+        e.style.display = "";
+    } else {
+        $("#toggleButton").text(toggleButtonTextOpen);
+        e.style.display = "none";
+    }
+  }
