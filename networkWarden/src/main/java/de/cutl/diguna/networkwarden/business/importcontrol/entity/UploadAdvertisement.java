4822fdef8bcb0b499ec4ecc4dad611db1c09c490
Chris <der.lange.christian@gmail.com>
2016-01-06 09:06:24 +0300

    added content for new advertisements

---
+++ networkWarden/src/main/java/de/cutl/diguna/networkwarden/business/importcontrol/entity/UploadAdvertisement.java
@@ -0,0 +1,12 @@
+package de.cutl.diguna.networkwarden.business.importcontrol.entity;
+
+import javax.enterprise.inject.Model;
+
+/**
+ *
+ * @author chris
+ */
+@Model
+public class UploadAdvertisement extends Upload {
+
+}
